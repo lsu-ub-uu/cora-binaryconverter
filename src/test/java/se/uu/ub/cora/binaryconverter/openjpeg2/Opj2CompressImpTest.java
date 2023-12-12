e596495369bd20207a4271610d8787f2f1f67557
marcus <marcus@65ec79ff7634>
2023-12-12 14:43:47 +0000

    Updated with some info regarding tiles and jpeg2000 and why to (probably) not use them in our setup

--- src/test/java/se/uu/ub/cora/binaryconverter/openjpeg2/Opj2CompressImpTest.java
+++ src/test/java/se/uu/ub/cora/binaryconverter/openjpeg2/Opj2CompressImpTest.java
@@ -5,11 +5,22 @@ import org.testng.annotations.Test;
 
 public class Opj2CompressImpTest {
 
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void test() {
 		// Settings lifted from iipimage (except -q that is from earlier alvin implementation):
-		// opj2_compress -i input.tif -o output.jp2 -q 25,28,30,35,40 (-r 2.5) -t 1024,1024 -n 7 -c
+		// opj2_compress -i input.tif -o output.jp2 -q 25,28,30,35,40 (-r 2.5) -n 7 -c
 		// "[256,256]" -b "64,64" -p RPCL -SOP -PLT -TLM -TP R
+		//
+		// "It is also possible to use tiling within JPEG2000, but the existence of precincts and
+		// code blocks largely replicates the features of the tiled pyramid structure used by TIFF
+		// making JPEG2000 tiling unnecessary and often even counter-productive for fast random
+		// access. Unlike with tiled pyramid TIFF, tiling with JPEG2000 is only applied to the full
+		// size image and not the sub-resolutions. Accessing a lower resolution, therefore, requires
+		// accessing every tile, which will make decoding speed slower. Moreover, it is not
+		// recommended to use tiling when using lossy JPEG2000 compression as compression artifacts
+		// can be visible at tile boundaries (compression artifacts are not an issue with lossless
+		// encoding)."
+		// If tiling is required -t 1024,1024
 
 		Opj2Ops opj2Ops = new Opj2OpsImp();
 		opj2Ops.inputPath(
@@ -18,7 +29,7 @@ public class Opj2CompressImpTest {
 				"/home/marcus/workspace/cora-fitnesse/FitNesseRoot/files/testResources/opj2output.jp2");
 		opj2Ops.codeBlockSize("64,64");
 		opj2Ops.precinctSize("256,256");
-		opj2Ops.tileSize("1024,1024");
+		// opj2Ops.tileSize("1024,1024");
 		opj2Ops.numOfResolutions(7);
 		opj2Ops.psnrQuality("25,28,30,35,40");
 		// opj2Ops.compressionRatio("2.5");
